d0d3986df1ab5cebe4de44803593c715ed1741d7
Suvradeep <suvradeep.paria829@gmail.com>
2020-09-12 12:45:25 +0530

    button disabled update

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 function Image(props) {
 
     const [ value, setValue ] = useState(false);
+    const [ button, setButton ] = useState(false);
     const url = props.data.post_url;
 
     let details;
@@ -19,17 +20,21 @@ function Image(props) {
     const downloadImage = () => {
     userInfo()
     if(value === true) {  
+        console.log('jj')
         axios({
-            url: url + '/download', //your url
+            // getting Cors error
+            // url: url + '/download', //original url
+            url: 'https://source.unsplash.com/random/500*500', // demo url
             method: 'GET',
             responseType: 'blob', // important
           }).then((response) => {
              const url = window.URL.createObjectURL(new Blob([response.data]));
              const link = document.createElement('a');
              link.href = url;
-             link.setAttribute('download', 'file.jpg'); //or any other extension
+             link.setAttribute('download', 'file.jpg'); 
              document.body.appendChild(link);
              link.click();
+             setButton(true)
           })
           .catch( error => {
                console.log(error)
@@ -42,7 +47,9 @@ function Image(props) {
         <div className="row">
             <div className="column">
                         <img src={url + '/download'} alt="Responsive" />
-                        <span>{props.data.author}</span><span><button onClick={downloadImage}>Download</button></span>
+                        <span>{props.data.author}</span>
+                        <span><button disabled={button} onClick={downloadImage}>Download</button></span>
+                        
             </div>   
          </div>  
         </div>
